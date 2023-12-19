5954c16e3f480852a8471abb2c28c3a5126707a4
Alejandro Hernandez <121833911+mkaJoe@users.noreply.github.com>
2023-12-18 21:20:27 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,250 @@
+// script.js 
+
+let questions = [ 
+	{ 
+		prompt: `¿Cual de estos te gustaria tener?`, 
+		options: [ 
+			"Bolso de marca", 
+			"Celular nuevo", 
+			"Un masaje", 
+			"Efectivo", 
+		], 
+		answer: "....", 
+	}, 
+
+	{ 
+		prompt: `ufff ok, neta.. a vers elige de nuevo.`, 
+		options: [ 
+			"Efectivo", 
+			"Celular nuevo", 
+			"Un masaje ",  
+		], 
+		answer: "....", 
+	}, 
+
+	{ 
+		prompt: `Va, supongamos que si, pero de cuanto?`, 
+		options: [ 
+			"$5,000 a $7,500", 
+			"$7,500 a $10,000", 
+			"$10,000 a $15,000", 
+			"$15,000 a $20,000", 
+		], 
+		answer: "....", 
+	}, 
+
+	{ 
+		prompt: `Chale, y si mejor te doy unos 10 varos y el masaje?`, 
+		options: ["Con final feliz", "sumale un six", "De artepil",], 
+		answer: ".....", 
+	}, 
+
+
+]; 
+
+// Get Dom Elements 
+
+let questionsEl = 
+	document.querySelector( 
+		"#questions"
+	); 
+let timerEl = 
+	document.querySelector("#timer"); 
+let choicesEl = 
+	document.querySelector("#options"); 
+let submitBtn = document.querySelector( 
+	"#submit-score"
+); 
+let startBtn = 
+	document.querySelector("#start"); 
+let nameEl = 
+	document.querySelector("#name"); 
+let feedbackEl = document.querySelector( 
+	"#feedback"
+); 
+let reStartBtn = 
+	document.querySelector("#restart"); 
+
+// Quiz's initial state 
+let currentQuestionIndex = 0; 
+let time = questions.length * 25; 
+let timerId; 
+
+// Start quiz and hide frontpage 
+
+function quizStart() { 
+	timerId = setInterval( 
+		clockTick, 
+		2000 
+	); 
+	timerEl.textContent = time; 
+	let landingScreenEl = 
+		document.getElementById( 
+			"start-screen"
+		); 
+	landingScreenEl.setAttribute( 
+		"class", 
+		"hide"
+	); 
+	questionsEl.removeAttribute( 
+		"class"
+	); 
+	getQuestion(); 
+} 
+
+// Loop through array of questions and 
+// Answers and create list with buttons 
+function getQuestion() { 
+	let currentQuestion = 
+		questions[currentQuestionIndex]; 
+	let promptEl = 
+		document.getElementById( 
+			"question-words"
+		); 
+	promptEl.textContent = 
+		currentQuestion.prompt; 
+	choicesEl.innerHTML = ""; 
+	currentQuestion.options.forEach( 
+		function (choice, i) { 
+			let choiceBtn = 
+				document.createElement( 
+					"button"
+				); 
+			choiceBtn.setAttribute( 
+				"value", 
+				choice 
+			); 
+			choiceBtn.textContent = 
+				i + 1 + ". " + choice; 
+			choiceBtn.onclick = 
+				questionClick; 
+			choicesEl.appendChild( 
+				choiceBtn 
+			); 
+		} 
+	); 
+} 
+
+// Check for right answers and deduct 
+// Time for wrong answer, go to next question 
+
+function questionClick() { 
+	if ( 
+		this.value !== 
+		questions[currentQuestionIndex] 
+			.answer 
+	) { 
+		time -= 0; 
+		if (time < 0) { 
+			time = 0; 
+		} 
+		timerEl.textContent = time; 
+		feedbackEl.textContent = `Pensando 
+		${questions[currentQuestionIndex].answer}.`; 
+		feedbackEl.style.color = "black"; 
+	} else { 
+		feedbackEl.textContent = 
+			"!!!"; 
+		feedbackEl.style.color = 
+			"black"; 
+	} 
+	feedbackEl.setAttribute( 
+		"class", 
+		"feedback"
+	); 
+	setTimeout(function () { 
+		feedbackEl.setAttribute( 
+			"class", 
+			"feedback hide"
+		); 
+	}, 2000); 
+	currentQuestionIndex++; 
+	if ( 
+		currentQuestionIndex === 
+		questions.length 
+	) { 
+		quizEnd(); 
+	} else { 
+		getQuestion(); 
+	} 
+} 
+
+// End quiz by hiding questions, 
+// Stop timer and show final score 
+
+function quizEnd() { 
+	clearInterval(timerId); 
+	let endScreenEl = 
+		document.getElementById( 
+			"quiz-end"
+		); 
+	endScreenEl.removeAttribute( 
+		"class"
+	); 
+	let finalScoreEl = 
+		document.getElementById( 
+			"score-final"
+		); 
+	finalScoreEl.textContent = time; 
+	questionsEl.setAttribute( 
+		"class", 
+		"hide"
+	); 
+} 
+
+// End quiz if timer reaches 0 
+
+function clockTick() { 
+	time--; 
+	timerEl.textContent = time; 
+	if (time <= 0) { 
+		quizEnd(); 
+	} 
+} 
+
+// Save score in local storage 
+// Along with users' name 
+
+function saveHighscore() { 
+	let name = nameEl.value.trim(); 
+	if (name !== "") { 
+		let highscores = 
+			JSON.parse( 
+				window.localStorage.getItem( 
+					"highscores"
+				) 
+			) || []; 
+		let newScore = { 
+			score: time, 
+			name: name, 
+		}; 
+		highscores.push(newScore); 
+		window.localStorage.setItem( 
+			"highscores", 
+			JSON.stringify(highscores) 
+		); 
+		alert( 
+			"Your Score has been Submitted"
+		); 
+	} 
+} 
+
+// Save users' score after pressing enter 
+
+function checkForEnter(event) { 
+	if (event.key === "Enter") { 
+		saveHighscore(); 
+		alert( 
+			"Your Score has been Submitted"
+		); 
+	} 
+} 
+nameEl.onkeyup = checkForEnter; 
+
+// Save users' score after clicking submit 
+
+submitBtn.onclick = saveHighscore; 
+
+// Start quiz after clicking start quiz 
+
+startBtn.onclick = quizStart;
